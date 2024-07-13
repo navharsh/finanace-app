730fbc081925842c4169a7a82c0d86fb2ff67dd3
Navharsh <navharshkumar12@gmail.com>
2024-07-13 09:00:35 +0530

    chore: Update dashboard page layout and add header component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,8 @@ import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
  return (
-  <UserButton afterSwitchSessionUrl="/" />
+  <div>Dashboard Page
+  </div>
+  
  )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
+  ClerkProvider
 } from '@clerk/nextjs'
 import "./globals.css";
 
